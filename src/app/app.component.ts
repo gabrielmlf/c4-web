4e5b1572b38ff43d0bf823345a5a1f9c05008200
Raira <rairasimoes@gmail.com>
2023-04-06 14:51:24 -0300

    v0.0.1 - minimal functions added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
-import { Component, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ResultScreenComponent } from './result-screen/result-screen.component';
+import { GameService } from './game.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'connect4';
-  restartGame = new EventEmitter();
+  restartGame = false;
+  options = { backdrop: 'static', focus: true, keyboard: false }
 
-  restart() {
-    this.restartGame.emit(true);
+  constructor(
+    private modalService: NgbModal,
+    private gameService: GameService
+  ) { }
+
+
+  ngOnInit(): void {
+   this.gameService.result.subscribe((player)=>{
+    console.log(`%cPlayer ${player} Wins`, 'color:green');
+    this.victoryModal(player)
+   })
+  }
+
+  victoryModal(player: any) {
+    const modalRef = this.modalService.open(ResultScreenComponent);
+    modalRef.componentInstance.player = player;
   }
 }
